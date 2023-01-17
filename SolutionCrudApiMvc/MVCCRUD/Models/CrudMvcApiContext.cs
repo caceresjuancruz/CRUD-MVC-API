21cdff8a572384b9ad89d7bf52cbcdb1c94a3cec
Jose Leon Caceres Musso <caceresjuan.dev@gmail.com>
2023-01-17 20:09:25 -0300

    ASP.NET Basic CRUD Web Api with EntityFramework

---
+++ SolutionCrudApiMvc/MVCCRUD/Models/CrudMvcApiContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCCRUD.Models;
+
+public partial class CrudMvcApiContext : DbContext
+{
+    public CrudMvcApiContext()
+    {
+    }
+
+    public CrudMvcApiContext(DbContextOptions<CrudMvcApiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+            => optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=CrudMvcApi;Trusted_Connection=True;Encrypt=False");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.Date)
+                .HasColumnType("date")
+                .HasColumnName("date");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("name");
+            entity.Property(e => e.Password)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ SolutionCrudApiMvc/MVCCRUD/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCCRUD.Models;
+
+public partial class Person
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string Password { get; set; } = null!;
+}
